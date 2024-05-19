1b912a99f1a9976a807f5c6ca71cf545717b0c1e
NgwangShalom <ngwangshalom956@gmail.com>
2024-05-19 15:23:49 +0100

    game

--- NumberGuessingGame.js
+++ NumberGuessingGame.js
@@ -1,13 +1,33 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const target = Math.floor(Math.random() * 100) + 1;
 let guess = 0;
 
-while (guess !== target) {
-    guess = parseInt(prompt("Guess a number between 1 and 100:"));
-    if (guess > target) {
-        console.log("Too high! Try again.");
-    } else if (guess < target) {
-        console.log("Too low! Try again.");
-    } else {
-        console.log("Congratulations! You guessed it right.");
-    }
+console.log("Guess a number between 1 and 100:");
+
+function askGuess() {
+    rl.question('Your guess: ', (answer) => {
+        guess = parseInt(answer);
+
+        if (isNaN(guess)) {
+            console.log("Please enter a valid number.");
+            askGuess();
+        } else if (guess > target) {
+            console.log("Too high! Try again.");
+            askGuess();
+        } else if (guess < target) {
+            console.log("Too low! Try again.");
+            askGuess();
+        } else {
+            console.log("Congratulations! You guessed it right.");
+            rl.close();
+        }
+    });
 }
+
+askGuess();
