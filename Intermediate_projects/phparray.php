c599a811a6a0a3db2bbabf3a9b41d12814e54ada
NgwangShalom <ngwangshalom956@gmail.com>
2024-06-04 15:29:49 +0100

    phparray

---
+++ Intermediate_projects/phparray.php
@@ -0,0 +1,15 @@
+<?php
+
+// simple multidimentional array in php code use it in your code lets work toegther
+$array = array(
+    array("shalom", "Ngwang", 29),
+    array("donald", "Fans", 20),
+    array("hepina", "Comp", 22),
+    array("const", "Hey", 10),
+    array("veteran", "Sugar", 30)
+);
+
+$array[0][2];
+$array[0][1];
+$array[1][2];
+$array[2][1];
