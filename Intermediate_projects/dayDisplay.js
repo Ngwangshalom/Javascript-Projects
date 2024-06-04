52887a8635be0fc21e360fe415b120b8c6e6a2c7
NgwangShalom <ngwangshalom956@gmail.com>
2024-06-04 15:29:42 +0100

    javaAtodays day

---
+++ Intermediate_projects/dayDisplay.js
@@ -0,0 +1,10 @@
+function displayDay(){
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    const todayDay = new Date();
+
+    const todaysDay = days[todayDay.getDay()];
+
+    console.log("the day of today is " + todaysDay);
+}
+displayDay();
